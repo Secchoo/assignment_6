d9c32a79795e8eabe0a09998f9d8fc8212dbe1b9
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-19 22:09:41 +0800

    finished code

---
+++ convert_to_uppercase.py
@@ -0,0 +1,9 @@
+def convert_to_uppercase():
+    #convert full name to uppercase
+    full_name = input("Enter your full name: ") #get full name from user
+    print(full_name.upper()) #convert full name to uppercase
+
+
+#call the function
+if __name__ == '__main__':
+    convert_to_uppercase()
