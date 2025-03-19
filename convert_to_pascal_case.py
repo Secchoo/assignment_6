a3cf0244740df6f1bff4e0dd40eef7628b07ec65
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-19 21:47:15 +0800

    wrong casing

---
+++ convert_to_pascal_case.py
@@ -0,0 +1,8 @@
+def convert_to_pascal_case():
+    #convert full name to pascal case
+    full_name = input("Enter your full name: ")
+    print(full_name.title())
+
+
+if __name__ == '__main__':
+    convert_to_pascal_case()
