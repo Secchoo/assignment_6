0511abc00562f0ff819fc0e78a9eedcdb943bfc4
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-19 22:00:34 +0800

    finished code

---
+++ convert_to_proper_case.py
@@ -0,0 +1,9 @@
+def convert_to_proper_case():
+    #convert full name to proper case
+    full_name = input("Enter your full name: ")
+    print(full_name.title())
+
+
+if __name__ == '__main__':
+    convert_to_proper_case()
+    
